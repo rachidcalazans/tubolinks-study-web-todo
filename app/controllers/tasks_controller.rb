3a985b40c8f4a0f84174001b618ecb97ba7e86b4
Tony Messias <tonyzrp@gmail.com>
2020-08-18 22:39:58 -0300

    Removes the show action and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,19 +5,12 @@ class TasksController < ApplicationController
     @new_task  = Task.new
   end
 
-  # @todo - use full turbolinks. Redirecting to index
-  #       - remove the create.js.erb file
-  #       - use redirect_to action: :index
   def create
     title = params[:task][:title]
 
     Task.new(title: title).tap(&:save!)
 
     redirect_to action: :index
-  end
-
-  def show
-    @task = Task.first
   end
 
   def destroy
